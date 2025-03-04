49a09d2b9c69231a978d11990cbe0b84522eaaec
Pedrosavioo <pedrosaviolima618@gmail.com>
2025-03-04 12:48:40 -0300

    feat: home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,25 @@
 import React from "react";
-import Button from './../../components/button/Button';
-import imageHome from "./../../imgs/image-home.svg";
-import './Home.css'
+import "./Home.css";
+import Navigate from "../../components/Navigate/Navigate";
+import HomeSession from "../../components/Sessions/Home/HomeSession";
+import About from "../../components/Sessions/About/About";
+import { useSelector } from "react-redux";
+import { useCurrentSection } from "../../redux/current-section";
+import Skills from "../../components/Sessions/Skills/Skills";
+import Projects from "../../components/Sessions/Projects/Projects";
+import Contact from "../../components/Sessions/Contact/Contact";
 
 const Home = () => {
+   const currentSession = useSelector(useCurrentSection);
+
    return (
       <>
-         <section id="home">
-            <div id="content-home">
-               <h1>Desenvolvedor de Software</h1>
-               <p>
-                  Transformando ideias em soluções digitais, com interfaces
-                  inteligentes, interações eficientes e aplicações web
-                  inovadoras que conectam tecnologia e experiência do usuário de
-                  forma impecável.
-               </p>
-               <Button content="Sobre mim" />
-            </div>
-            <div id="div-img">
-               <img src={imageHome} alt="developer" />
-            </div>
-         </section>
+         <Navigate />
+         {currentSession === 0 && <HomeSession />}
+         {currentSession === 1 && <About />}
+         {currentSession === 2 && <Skills />}
+         {currentSession >= 3 && currentSession < 4 && <Projects />}
+         {currentSession === 4 && <Contact />}
       </>
    );
 };
