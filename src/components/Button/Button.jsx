518911e5f32fe628b3155acd0d9f2ebffc05fbaf
Pedrosavioo <pedrosaviolima618@gmail.com>
2025-03-04 12:49:30 -0300

    feat: add button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import "./Button.css";
 import lineImg from "./../../imgs/line.svg";
 
-const Button = ({ content, onClick }) => {
+const Button = ({ content, onClick, className }) => {
+   const classNameString = `btn ${className}`;
    return (
-      <button className="btn" onClick={onclick}>
+      <button className={classNameString} onClick={onclick}>
          {content}
          <img src={lineImg} alt="line button" />
       </button>
