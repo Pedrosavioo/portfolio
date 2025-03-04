f9b8903bb471d2875c8649250a375aad7d6577ff
Pedrosavioo <pedrosaviolima618@gmail.com>
2025-03-04 12:49:09 -0300

    feat: layout default

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header/Header";
+import {
+   nextSection,
+   previousSection,
+   useCurrentSection,
+} from "../redux/current-section";
+import { useDispatch, useSelector } from "react-redux";
+import {
+   resetAnimation,
+   scrollBottom,
+   scrollTop,
+} from "../redux/animations.-scroll";
+import { debounce } from "lodash";
 
 const Layout = () => {
+   const dispatch = useDispatch();
+
+   // Função para lidar com o evento de scroll
+   const handleScroll = debounce((event) => {
+      if (event.deltaY > 0) {
+         // Scroll para baixo
+         dispatch(scrollBottom());
+
+         setTimeout(() => {
+            dispatch(nextSection());
+            dispatch(resetAnimation());
+         }, 1000);
+      }
+      if (event.deltaY < 0) {
+         // Scroll para cima
+         dispatch(scrollTop());
+
+         setTimeout(() => {
+            dispatch(previousSection());
+            dispatch(resetAnimation());
+         }, 1000);
+      }
+   }, 300); // Delay de 300ms entre chamadas
+
+   useEffect(() => {
+      // Adiciona o evento ao montar o componente
+      window.addEventListener("wheel", handleScroll);
+
+      return () => {
+         // Remove o evento ao desmontar o componente
+         window.removeEventListener("wheel", handleScroll);
+      };
+   }, [handleScroll]); // Adiciona a dependência da função
+
    return (
       <>
          <Header />
